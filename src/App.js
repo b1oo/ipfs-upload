9bf5c3f6932473756c8cc05d8d568963c3127b7d
Sergio <sergiobluez@gmail.com>
2023-02-16 14:05:45 -0800

    IPFS Uploader
    IPFS Uploaded made with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { create } from 'ipfs-http-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ipfs = create({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+
+class App extends React.Component {
+  state = {
+    file: null,
+    ipfsHash: null,
+  };
+
+  handleFileUpload = (event) => {
+    this.setState({ file: event.target.files[0] });
+  };
+
+  handleUpload = async () => {
+    const { file } = this.state;
+
+    if (!file) {
+      return;
+    }
+
+    const buffer = await file.arrayBuffer();
+    const result = await ipfs.add(buffer);
+    this.setState({ ipfsHash: result.cid.toString() });
+  };
+
+  render() {
+    const { file, ipfsHash } = this.state;
+
+    return (
+      <div>
+        <h1>IPFS Image Uploader</h1>
+        <input type="file" onChange={this.handleFileUpload} />
+        <button onClick={this.handleUpload}>Upload to IPFS</button>
+        {ipfsHash && (
+          <div>
+            <p>IPFS Hash:</p>
+            <a href={`https://ipfs.io/ipfs/${ipfsHash}`}>{ipfsHash}</a>
+          </div>
+          
+        )}
+      </div>
+      
+    );
+  }
 }
 
 export default App;
